69aa08258cdd6a1d62d914d448b333373c5d95aa
behnam22048 <Behnammahmodei@gmail.com>
2022-03-24 22:44:02 +0430

    Add another line to print more.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 name = input('Enter Your name Please: ')
 print(f'Hi {name}, Today we gonna talk about Python... ')
-
+print(f'{name}After That we gonna talk about you Fucking damn pretty Shirts..')
 if __name__ == "__main__":
     pass
